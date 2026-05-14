7dc10f2b2fec738f1cb13108c59c847c30da79eb
SEROBC <seeobcholakyan3@gmail.com>
2026-05-14 10:01:49 -0700

    Create brackets.js

---
+++ backend/tournament-service/brackets.js
@@ -0,0 +1,10 @@
+class BracketMatch {
+
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.winner = null;
+    }
+}
+
+module.exports = BracketMatch;
